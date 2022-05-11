5f89ed80f6fc76128b3af6afb102e47d26eab174
CK-Austin <caustin2@student.fullsail.edu>
2022-05-11 13:17:00 -0400

    Add project files.

---
+++ SWE_PersonClass/People.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class People
+{
+public:
+   std::string fName;
+   std::string LName;
+   std::string address;
+   std::string city;
+   std::string state;
+   std::string zipCode;
+
+};
+
---
+++ SWE_PersonClass/SWE_PersonClass.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello user, please enter a person's information: ";
+    
+  
+    std::cout << "Please enter first name: ";
+   
+}
+
+
+
